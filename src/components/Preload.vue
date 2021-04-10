19bdf7f660957de92a15b0aa7628abc1051420d6
S-codes14 <smlmnguni14@gmail.com>
2021-04-10 12:29:29 +0200

    first commit

---
+++ src/components/Preload.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="loader" v-once>
+    <div class="loader__content">
+
+
+      <p class="loader__tip">
+        {{ messages | randomize }}
+      </p>
+
+      <div class="spinner loader__spinner"></div>
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'preload',
+  data() {
+    return {
+      messages: [
+        'Use the left and right arrow keys to quickly navigate between gradients - gradientful',
+        'Use the top and bottom arrow keys to change gradient direction - gradientful',
+        'Click the colored square next to hex value to copy to clipboard - gradientful',
+        'Hit the shift key to see all gradients - gradientful',
+        'Press the enter key to get the css code for the gradient - gradientful',
+      ],
+    };
+  },
+  filters: {
+    randomize(messages) {
+      if (!messages) return '';
+      const randomMessage = Math.floor(Math.random() * messages.length);
+      return messages[randomMessage];
+    },
+  },
+};
+</script>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <header class="header" v-once>
+    <div class="header__branding">
+    Gradientful
+        </div>
+      </a>
+    </div>
+
+    <ul class="social header__social">
+      <li>
+        <a
+          href="#" class="button button--twitter" @click.prevent="shareOnTwitter()">
+          <Twitter class="button__icon" />
+          <span>Share on Twitter</span>
+        </a>
+      </li>
+      <li class="ml10">
+        <a href="#" class="button button--facebook" @click.prevent="shareOnFacebook()">
+          <Facebook class="button__icon" />
+          <span>Share on Facebook</span>
+        </a>
+      </li>
+      <li class="ml10">
+        <a href="https://github.com/S-codes14/gradientful" target="_blank" @click="goToGithub()">
+          <Github class="header__github" />
+        </a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+import Plus from '../assets/plus.svg';
+import Arrow from '../assets/arrow.svg';
+import Logo from '../assets/logo.svg';
+import Facebook from '../assets/facebook.svg';
+import Twitter from '../assets/twitter.svg';
+import Github from '../assets/github.svg';
+import Skillshare from '../assets/skillshare.svg';
+
+export default {
+  name: 'topbar',
+  components: {
+    Arrow,
+    Plus,
+    Logo,
+    Facebook,
+    Twitter,
+    Github,
+    Skillshare,
+  },
+  methods: {
+    goToSkillshare() {
+      this.$ga.event('sponsor', 'click', 'Skillshare');
+    },
+    goToGithub() {
+      this.$ga.event('social', 'click', 'github');
+    },
+    shareOnTwitter() {
+      this.$ga.social('twitter', 'share', 'http://gradientful.netlify.app/');
+      window.open(
+        'http://twitter.com/share?url=https://gradientful.netlify.app&amp;text=Beautiful%20color%20gradients%20for%20designers%20and%20developers&amp;via=Sibongu14624275',
+        'twitter-share',
+        'width=700,height=300',
+      );
+    },
+    shareOnFacebook() {
+      this.$ga.social('facebook', 'share', 'http://gradientful.netlify.app/');
+      window.open(
+        'http://www.facebook.com/sharer.php?u=https://gradientful.netlify.app',
+        'facebook-share',
+        'width=700,height=400',
+      );
+    },
+  },
+};
+</script>
